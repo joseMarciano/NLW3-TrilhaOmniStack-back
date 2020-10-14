fa47be18b76aef45ad2482dc3262060cb9d44ef4
zezeka <marcianojosepaulo@gmail.com>
2020-10-14 20:46:56 -0300

    Criando end-point para cadastro de orfanato

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,37 @@
 import express from 'express';
 import './database/connection'; // importando o arquivo de conexão do DB para o server
 
+import {getRepository} from 'typeorm'; // typeorm utiliza o pattern 'repositoryPattern' que diz que toda operação feita passa por um repositório... o repositório detem a regra de como o dado pode ser criado,deletado etc..
+import Orphanages from './models/Orphanage'; //pegando a entidade 
+
 const app = express(); //criando a aplicação 
 app.use(express.json());//express por padrão não entende json então eu faço com que ele interprete json com a função express.json()
 
-app.get('/users',(request,response) =>{
+app.post('/orphanages', async(request,response) =>{
+    const { /* desestruturando o body da requisição e m variáveis  */
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    } = request.body;
+
+    const orphanagesRepository = getRepository(Orphanages); // a partir de agora eu tenho os metodos para frazer um CRUD
+
+    const orphanage = orphanagesRepository.create({ // cria o orfanato... mas não salva
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    });
+
+    await orphanagesRepository.save(orphanage);
+
     return response.json({message:"Hello"});
 });
 

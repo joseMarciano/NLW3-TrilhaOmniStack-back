e479c8b1c7be30e3e28bda5ae22904e892ab6285
zezeka <marcianojosepaulo@gmail.com>
2020-10-15 19:21:02 -0300

    Instalando a dependência cors para não dar erro de CORS

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,11 @@ import './database/connection'; // importando o arquivo de conexão do DB para o
 import path from 'path'; 
 import routes from './routes'
 import errorHandler from './errors/handler';
+import cors from 'cors'
 
 const app = express(); //criando a aplicação 
+
+app.use(cors()); //evitando erro CORS
 app.use(express.json());//express por padrão não entende json então eu faço com que ele interprete json com a função express.json()
 app.use(routes);
 app.use('/uploads',express.static(path.join(__dirname, '..', 'uploads'))); // aplicando os endereços das imagens no servidor para eu poder acessar

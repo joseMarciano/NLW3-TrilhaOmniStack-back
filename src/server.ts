9b15f3fac377fab088ebd79da16c4e2c55e5898d
zezeka <marcianojosepaulo@gmail.com>
2020-10-15 18:27:06 -0300

    Adicionando a rota para conseguir acessar as imanes

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 
 import express from 'express';
 import './database/connection'; // importando o arquivo de conexão do DB para o server
-
+import path from 'path'; 
 import routes from './routes'
 
 const app = express(); //criando a aplicação 
 app.use(express.json());//express por padrão não entende json então eu faço com que ele interprete json com a função express.json()
 app.use(routes);
-
+app.use('/uploads',express.static(path.join(__dirname, '..', 'uploads'))); // aplicando os endereços das imagens no servidor para eu poder acessar
 
 app.listen(3333);

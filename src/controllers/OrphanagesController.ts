fa75873c1661c79df7f06c84a6b2d75a7f395c0e
zezeka <marcianojosepaulo@gmail.com>
2020-10-15 19:18:13 -0300

    Criando a validação de dados para criação de orfanato

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -3,6 +3,7 @@ import {Request,Response} from 'express'; // importando o tipo de request e resp
 import {getRepository} from 'typeorm'; // typeorm utiliza o pattern 'repositoryPattern' que diz que toda operação feita passa por um repositório... o repositório detem a regra de como o dado pode ser criado,deletado etc..
 import Orphanages from '../models/Orphanage'; //pegando a entidade 
 import orphanageView from '../views/orphanages_view';
+import * as Yup from 'yup';
 export default {
 
     async listAll(request: Request, response: Response){
@@ -42,7 +43,7 @@ export default {
              return {path:image.filename};
         });
 
-        const orphanage = orphanagesRepository.create({ // cria o orfanato... mas não salva
+        const data = {            
             name,
             latitude,
             longitude,
@@ -51,7 +52,32 @@ export default {
             opening_hours,
             open_on_weekends,
             images
+        };
+        const schema = Yup.object().shape({ // quais os tipos de dados esperados em cada propriedade
+            name: Yup.string().required(),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array( //images é um array de objetos e eu também faço a validação dentro do array
+                Yup.object().shape({
+                    path: Yup.string().required()
+                })
+            )
         });
+
+        //Fazendo a validação 
+        await schema.validate(data,{
+            abortEarly: false /* retornando todos os erros, sem essa propriedade, 
+                                 é retornado o erro de forma antecipada, ou seja, 
+                                 o primeiro erro encontrado já vai ser barrado na validação.
+                                 Dessa forma se tenho 4 campos invalidos, será retornado os 4 campos invalidos e não somente o primeiro
+                                */
+        })
+
+        const orphanage = orphanagesRepository.create(data);
     
         await orphanagesRepository.save(orphanage);
     
--- src/errors/handler.ts
+++ src/errors/handler.ts
@@ -1,10 +1,28 @@
 /* Criando um exception handler */
 
 import { ErrorRequestHandler} from 'express'; //Importando uma tipagem padrão para qualquer error handler que eu for criar
+import { ValidationError } from 'yup';
 
+interface ValidationErrors {
+    [key: string]: string[];
+}
 // a const errorHandler deve ser sempre do tipo ErrorRequestHandler... sendo assim, os parâmetros da minhas função ja estarão tipados
 const errorHandler:ErrorRequestHandler = (error,request,response,next) => {
-    console.log(error);
+
+    //se for um erro de validação vamos retornar o erro de outra maneira
+    if(error instanceof ValidationError){
+        let errors: ValidationErrors = {}
+
+        error.inner.forEach(error =>{
+            errors[error.path] = error.errors;
+        });
+
+        return response.status(400).json({
+            message: "Validation fails",
+            date: new Date(),
+            errors
+        })
+    }
 
     return response.status(500).json({
         message: "Internal server error",
